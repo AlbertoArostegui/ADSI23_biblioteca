3bf0d55d5a14b16c36af7f4853f1dc3d5b02780f
unaigarcia02 <ugarcia060@ikasle.ehu.eus>
2023-12-30 13:35:11 +0100

    hola

--- controller/LibraryController.py
+++ controller/LibraryController.py
@@ -1,50 +1,63 @@
+import sqlite3
+from datetime import datetime
+import _sqlite3 as sql
 from model import Connection, Book, User
 from model.tools import hash_password
 
 db = Connection()
 
+
 class LibraryController:
-	__instance = None
+    __instance = None
 
-	def __new__(cls):
-		if cls.__instance is None:
-			cls.__instance = super(LibraryController, cls).__new__(cls)
-			cls.__instance.__initialized = False
-		return cls.__instance
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super(LibraryController, cls).__new__(cls)
+            cls.__instance.__initialized = False
+        return cls.__instance
 
+    def get_book_by_id(self, book_id):
+        book_data = db.select("SELECT * FROM Book WHERE id = ?", (book_id,))
+        if book_data:
+            # Suponiendo que la tabla Book tiene columnas id, title, author, etc.
+            return Book(book_data[0][0], book_data[0][1], book_data[0][2], book_data[0][3], book_data[0][4])
+        else:
+            return None
 
-	def search_books(self, title="", author="", limit=6, page=0):
-		count = db.select("""
-				SELECT count() 
-				FROM Book b, Author a 
-				WHERE b.author=a.id 
-					AND b.title LIKE ? 
-					AND a.name LIKE ? 
-		""", (f"%{title}%", f"%{author}%"))[0][0]
-		res = db.select("""
-				SELECT b.* 
-				FROM Book b, Author a 
-				WHERE b.author=a.id 
-					AND b.title LIKE ? 
-					AND a.name LIKE ? 
-				LIMIT ? OFFSET ?
-		""", (f"%{title}%", f"%{author}%", limit, limit*page))
-		books = [
-			Book(b[0],b[1],b[2],b[3],b[4])
-			for b in res
-		]
-		return books, count
+    def search_books(self, title="", author="", limit=6, page=0):
+        count = db.select("""
+                SELECT count() 
+                FROM Book b, Author a 
+                WHERE b.author=a.id 
+                    AND b.title LIKE ? 
+                    AND a.name LIKE ? 
+        """, (f"%{title}%", f"%{author}%"))[0][0]
+        res = db.select("""
+                SELECT b.* 
+                FROM Book b, Author a 
+                WHERE b.author=a.id 
+                    AND b.title LIKE ? 
+                    AND a.name LIKE ? 
+                LIMIT ? OFFSET ?
+        """, (f"%{title}%", f"%{author}%", limit, limit * page))
+        books = [
+            Book(b[0], b[1], b[2], b[3], b[4])
+            for b in res
+        ]
+        return books, count
 
-	def get_user(self, email, password):
-		user = db.select("SELECT * from User WHERE email = ? AND password = ?", (email, hash_password(password)))
-		if len(user) > 0:
-			return User(user[0][0], user[0][1], user[0][2])
-		else:
-			return None
+    def get_user(self, email, password):
+        user = db.select("SELECT * from User WHERE email = ? AND password = ?", (email, hash_password(password)))
+        if len(user) > 0:
+            return User(user[0][0], user[0][1], user[0][2])
+        else:
+            return None
 
-	def get_user_cookies(self, token, time):
-		user = db.select("SELECT u.* from User u, Session s WHERE u.id = s.user_id AND s.last_login = ? AND s.session_hash = ?", (time, token))
-		if len(user) > 0:
-			return User(user[0][0], user[0][1], user[0][2])
-		else:
-			return None
+    def get_user_cookies(self, token, time):
+        user = db.select(
+            "SELECT u.* from User u, Session s WHERE u.id = s.user_id AND s.last_login = ? AND s.session_hash = ?",
+            (time, token))
+        if len(user) > 0:
+            return User(user[0][0], user[0][1], user[0][2])
+        else:
+            return None
--- controller/webServer.py
+++ controller/webServer.py
@@ -1,72 +1,104 @@
-from .LibraryController import LibraryController
-from flask import Flask, render_template, request, make_response, redirect
+from datetime import datetime, timedelta
+from model.Connection import Connection
+
+from controller.LibraryController import LibraryController
+from flask import Flask, render_template, request, make_response, redirect, url_for
+
+import sqlite3
 
 app = Flask(__name__, static_url_path='', static_folder='../view/static', template_folder='../view/')
-
 
 library = LibraryController()
 
 
 @app.before_request
 def get_logged_user():
-	if '/css' not in request.path and '/js' not in request.path:
-		token = request.cookies.get('token')
-		time = request.cookies.get('time')
-		if token and time:
-			request.user = library.get_user_cookies(token, int(time))
-			if request.user:
-				request.user.token = token
+    if '/css' not in request.path and '/js' not in request.path:
+        token = request.cookies.get('token')
+        time = request.cookies.get('time')
+        if token and time:
+            request.user = library.get_user_cookies(token, int(time))
+            if request.user:
+                request.user.token = token
 
 
 @app.after_request
 def add_cookies(response):
-	if 'user' in dir(request) and request.user and request.user.token:
-		session = request.user.validate_session(request.user.token)
-		response.set_cookie('token', session.hash)
-		response.set_cookie('time', str(session.time))
-	return response
+    if 'user' in dir(request) and request.user and request.user.token:
+        session = request.user.validate_session(request.user.token)
+        response.set_cookie('token', session.hash)
+        response.set_cookie('time', str(session.time))
+    return response
 
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route('/catalogue')
 def catalogue():
-	title = request.values.get("title", "")
-	author = request.values.get("author", "")
-	page = int(request.values.get("page", 1))
-	books, nb_books = library.search_books(title=title, author=author, page=page - 1)
-	total_pages = (nb_books // 6) + 1
-	return render_template('catalogue.html', books=books, title=title, author=author, current_page=page,
-	                       total_pages=total_pages, max=max, min=min)
+    title = request.values.get("title", "")
+    author = request.values.get("author", "")
+    page = int(request.values.get("page", 1))
+    books, nb_books = library.search_books(title=title, author=author, page=page - 1)
+    total_pages = (nb_books // 6) + 1
+    return render_template('catalogue.html', books=books, title=title, author=author, current_page=page,
+                           total_pages=total_pages, max=max, min=min)
+
+
+@app.route('/reserva_exitosa')
+def reserva_exitosa():
+    return render_template('reserva_exitosa.html')
+
+
+@app.route('/reserve-book', methods=['POST'])
+def reserve_book():
+    insertar = Connection()
+    user_id = request.form.get('user_id')
+    book_id = request.form.get('book_id')
+
+    fecha_inicio = datetime.now()
+    fecha_fin = fecha_inicio + timedelta(days=60)
+    fecha_ini_str = fecha_inicio.strftime('%Y-%m-%d')
+    fecha_fin_str = fecha_fin.strftime('%Y-%m-%d')
+
+    # Preparar los parámetros para la consulta SQL como una tupla
+    p = (user_id, book_id, fecha_ini_str, fecha_fin_str)
+
+    # Pasar la sentencia SQL con marcadores de estilo de SQLite y la tupla de parámetros al método insert
+    if insertar.insert("INSERT INTO reserva (user_id, book_id, fecha_inicio, fecha_fin) VALUES (?, ?, ?, ?)", p):
+        # Reserva exitosa, redirigir o mostrar un mensaje
+        return redirect('reserva_exitosa')
+    else:
+        # Error en la reserva, manejar adecuadamente
+        return "Error en la reserva", 400
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if 'user' in dir(request) and request.user and request.user.token:
-		return redirect('/')
-	email = request.values.get("email", "")
-	password = request.values.get("password", "")
-	user = library.get_user(email, password)
-	if user:
-		session = user.new_session()
-		resp = redirect("/")
-		resp.set_cookie('token', session.hash)
-		resp.set_cookie('time', str(session.time))
-	else:
-		resp = make_response(render_template('login.html'))
-	return resp
+    if 'user' in dir(request) and request.user and request.user.token:
+        return redirect('/')
+    email = request.values.get("email", "")
+    password = request.values.get("password", "")
+    user = library.get_user(email, password)
+    if user:
+        session = user.new_session()
+        resp = redirect("/")
+        resp.set_cookie('token', session.hash)
+        resp.set_cookie('time', str(session.time))
+    else:
+        resp = make_response(render_template('login.html'))
+    return resp
 
 
 @app.route('/logout')
 def logout():
-	path = request.values.get("path", "/")
-	resp = redirect(path)
-	resp.delete_cookie('token')
-	resp.delete_cookie('time')
-	if 'user' in dir(request) and request.user and request.user.token:
-		request.user.delete_session(request.user.token)
-		request.user = None
-	return resp
+    path = request.values.get("path", "/")
+    resp = redirect(path)
+    resp.delete_cookie('token')
+    resp.delete_cookie('time')
+    if 'user' in dir(request) and request.user and request.user.token:
+        request.user.delete_session(request.user.token)
+        request.user = None
+    return resp
